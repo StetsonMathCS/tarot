e458a7fec507ebf68bfa849a07406c11dbc01237
kevin <kevin.riedl.privat@gmail.com>
2018-10-10 19:50:43 +0200

    adapted dockerfiles etc.

--- docker_start.sh
+++ docker_start.sh
@@ -1,4 +1,4 @@
-docker pull wsdt/stetson_tarot:latest
+docker pull wsdt/tarot:latest
 docker stop stetson_server
 docker rm stetson_server
-docker run --name stetson_server -p 80:8080 wsdt/stetson_tarot:latest
+docker run --name stetson_server -p 80:8080 -v /home/kevin/logs:/usr/local/tomcat/logs wsdt/tarot:latest
